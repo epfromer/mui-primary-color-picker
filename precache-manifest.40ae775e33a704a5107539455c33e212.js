0ad8ca6b17aba8f6dd97863e111a386539342702
Ed Pfromer <epfromer@gmail.com>
2020-04-18 15:24:49 -0600

    Updates

---
+++ precache-manifest.40ae775e33a704a5107539455c33e212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5433db8b6caad520e1710328343b9127",
+    "url": "/mui-primary-color-picker/index.html"
+  },
+  {
+    "revision": "fe13ffd6c51bd6e9ddd8",
+    "url": "/mui-primary-color-picker/static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "abce9d60a4375841504c",
+    "url": "/mui-primary-color-picker/static/js/2.4dab0451.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mui-primary-color-picker/static/js/2.4dab0451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe13ffd6c51bd6e9ddd8",
+    "url": "/mui-primary-color-picker/static/js/main.c059a7d1.chunk.js"
+  },
+  {
+    "revision": "406afa73d93cbf3766ff",
+    "url": "/mui-primary-color-picker/static/js/runtime-main.e5c17f0e.js"
+  }
+]);
